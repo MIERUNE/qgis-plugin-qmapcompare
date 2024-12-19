6dcad8d75440d5267c891bb648d54974d1754860
bordoray <bordoray@gmail.com>
2024-12-19 16:30:12 +0900

    Plug button signals

--- qmapcompare_dockwidget.py
+++ qmapcompare_dockwidget.py
@@ -11,11 +11,24 @@ class QMapCompareDockWidget(QDockWidget):
             os.path.join(os.path.dirname(__file__), "qmapcompare_dockwidget.ui"), self
         )
 
-        # self.ui.pushButton_run.clicked.connect(self.get_and_show_input_text)
+        self.ui.pushButton_mirror.clicked.connect(self._on_pushbutton_mirror_clicked)
+        self.ui.pushButton_swipe.clicked.connect(self._on_pushbutton_swipe_clicked)
+        self.ui.pushButton_lens.clicked.connect(self._on_pushbutton_lens_clicked)
+        self.ui.pushButton_stopcompare.clicked.connect(self._on_pushbutton_stopcompare_clicked)
         # self.ui.pushButton_cancel.clicked.connect(self.close)
 
-    def get_and_show_input_text(self):
-        # テキストボックス値取得
-        text_value = self.ui.lineEdit.text()
-        # テキストボックス値をメッセージ表示
-        QMessageBox.information(None, "ウィンドウ名", text_value)
+    # TODO: implement
+    def _on_pushbutton_mirror_clicked(self):
+        QMessageBox.information(None, "Message", "Mirror")
+    
+    # TODO: implement
+    def _on_pushbutton_swipe_clicked(self):
+        QMessageBox.information(None, "Message", "Swipe")
+    
+    # TODO: implement
+    def _on_pushbutton_lens_clicked(self):
+        QMessageBox.information(None, "Message", "Lens")
+    
+    # TODO: implement
+    def _on_pushbutton_stopcompare_clicked(self):
+        QMessageBox.information(None, "Message", "Stop!")
