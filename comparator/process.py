389ee833c7400409f4e159a3c3d9c6a0d3f1209d
bordoray <bordoray@gmail.com>
2024-12-25 16:39:38 +0900

    Create geometry generator

--- comparator/process.py
+++ comparator/process.py
@@ -1,17 +1,40 @@
-from qgis.core import QgsProject, QgsLayerTreeGroup, QgsVectorLayer
+from qgis.core import QgsProject, QgsLayerTreeGroup, QgsVectorLayer, QgsGeometryGeneratorSymbolLayer, QgsFillSymbol
 
 def compare_split(compare_layers):
-    compare_layer_group = _create_compare_layer_group_and_mask()
+    compare_layer_group, compare_mask_layer = _create_compare_layer_group_and_mask()
 
     # move target compare layers to layer group
     for layer in compare_layers:
         compare_layer_group.addLayer(layer)
 
     # TODO: Symbolize with geometry generator
+    formula = """
+    make_rectangle_3points(
+        make_point(x(@map_extent_center), y(@map_extent_center) - (@map_extent_height / 2)),
+        make_point(x(@map_extent_center), y(@map_extent_center) + (@map_extent_height / 2)),
+        make_point(x(@map_extent_center) - (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
+        0
+    )
+    """
+
+    # Step 3: Create a Geometry Generator Symbol Layer
+    geometry_generator = QgsGeometryGeneratorSymbolLayer.create({
+        'geometryModifier': formula,
+        'geometry_type': 2,  # Polygon
+        'extent': ''  # Use the default map extent
+    })
+
+    # Step 4: Create a symbol and apply the geometry generator
+    symbol = QgsFillSymbol.createSimple({'color': '255,0,0,50'})  # Red fill with transparency
+    symbol.changeSymbolLayer(0, geometry_generator)
+
+    # Step 5: Apply the symbol to the scratch layer
+    compare_mask_layer.renderer().setSymbol(symbol)
+
 
     return
 
-def _create_compare_layer_group_and_mask() -> QgsLayerTreeGroup:
+def _create_compare_layer_group_and_mask() :
     """Create layer group with mask at the top
     of layer tree"""
     project = QgsProject.instance()
@@ -22,16 +45,14 @@ def _create_compare_layer_group_and_mask() -> QgsLayerTreeGroup:
     root.insertChildNode(0, layer_group) 
 
     # Create a scratch polygon layer
-    scratch_layer = QgsVectorLayer("Polygon?crs=EPSG:3857", "QMapCompare_mask", "memory")
-    if not scratch_layer.isValid():
+    mask_layer = QgsVectorLayer("Polygon?crs=EPSG:3857", "QMapCompare_mask", "memory")
+    if not mask_layer.isValid():
         print("Failed to create the scratch layer")
     else:
-        # Step 3: Add the scratch layer to the project
-        project.addMapLayer(scratch_layer, False)  # Add without inserting into the default layer tree
+        # Add polygon layer to compare layer group
+        project.addMapLayer(mask_layer, False)  # Add without inserting into the default layer tree
+        layer_group.addLayer(mask_layer)
 
-        # Step 4: Add the layer to the newly created group
-        layer_group.addLayer(scratch_layer)
-
-    return layer_group
+    return layer_group, mask_layer
 
 
