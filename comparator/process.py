bdcb6083c5dc4c75c6ee62d7206a3766ecca7d1d
bordoray <bordoray@gmail.com>
2025-01-18 16:21:22 +0900

    Naming convention

--- comparator/process.py
+++ comparator/process.py
@@ -1,6 +1,7 @@
 from qgis.core import (
     QgsProject,
     QgsLayerTreeGroup,
+    QgsMapLayer,
     QgsVectorLayer,
     QgsGeometryGeneratorSymbolLayer,
     QgsFillSymbol,
@@ -14,23 +15,29 @@ from qgis.PyQt.QtGui import QPainter
 from .constants import compare_mask_layer_name, compare_group_name
 
 
-def compare_split(compare_layers):
+def compare_split(compare_layers: list) -> None:
+    """
+    Make QGIS Map to be in compare split mode 
+    with input compare layers 
+    """
+
     compare_layer_group, compare_mask_layer = _create_compare_layer_group_and_mask()
 
-    # move target compare layers to layer group
+    # Add target compare layers to layer group
     for layer in compare_layers:
         # get layer origin node
         root = QgsProject.instance().layerTreeRoot()
         layer_node = root.findLayer(layer.id())
         
-        # move layer to group (add to group and remove origin node)
-        # Don't add if it is already in compare group
-        if not _is_in_group(layer, compare_layer_group):
+        # Hide target layer to be shown in Compare Group
+        layer_node.setItemVisibilityChecked(False)
+        
+        # Add layer to compare group if not existing
+        if not _is_in_group(layer, compare_layer_group): 
             compare_layer_group.addLayer(layer)
-            root.removeChildNode(layer_node) 
     
 
-    # Symbolize with geometry generator
+    # Symbolize mask with geometry generator
     formula = """make_rectangle_3points(
         make_point(x(@map_extent_center), y(@map_extent_center) - (@map_extent_height / 2)),
         make_point(x(@map_extent_center), y(@map_extent_center) + (@map_extent_height / 2)),
@@ -41,13 +48,13 @@ def compare_split(compare_layers):
         {
             "geometryModifier": formula,
             "geometry_type": 2,  # Polygon
-            "extent": "",  # Use the default map extent
+            "extent": "",  
         }
     )
 
     symbol = QgsFillSymbol.createSimple(
         {"color": "255,0,0,50"}
-    )  # Red fill with transparency
+    )  
     symbol.changeSymbolLayer(0, geometry_generator)
 
     #  Create the inverted polygon renderer
@@ -60,12 +67,17 @@ def compare_split(compare_layers):
     return
 
 
-def _create_compare_layer_group_and_mask():
-    """Create layer group with mask at the top
-    of layer tree return layer_group and compare_mask_layer"""
+def _create_compare_layer_group_and_mask()-> tuple[QgsLayerTreeGroup,QgsMapLayer]:
+    """Create layer group with mask layer inside at the top
+    of layer tree return layer_group and compare_mask_layer
+    Output: 
+    - layer_group_node: Compare Layer Group, 
+    - mask_layer: Compare mask layer 
+    """
+
     project = QgsProject.instance()
     root = project.layerTreeRoot()
-
+    
     # Create a scratch polygon layer
     mask_layers =  project.mapLayersByName(compare_mask_layer_name)
     if mask_layers:
@@ -80,28 +92,23 @@ def _create_compare_layer_group_and_mask():
                 mask_layer, False
             )  
             
-
-    layer_group_node = root.findGroup(compare_group_name)
-    if not layer_group_node:
-        # create compare layer group to the top of layer treee
-        options = QgsGroupLayer.LayerOptions(QgsCoordinateTransformContext())
-        group_layer = QgsGroupLayer('group', options)
-        group_layer.setChildLayers([mask_layer])
-
-        project.addMapLayer(group_layer, False)
-        layer_group_node = QgsLayerTreeGroup(compare_group_name)
-        layer_group_node.setGroupLayer(group_layer)
-
-        # group_layer.triggerRepaint()
-        layer_group_node.addLayer(mask_layer)
-        root.insertChildNode(0, layer_group_node)
         
+    # create compare layer group to the top of layer treee
+    options = QgsGroupLayer.LayerOptions(QgsCoordinateTransformContext())
+    group_layer = QgsGroupLayer('group', options)
+    group_layer.setChildLayers([mask_layer])
 
+    project.addMapLayer(group_layer, False)
+    layer_group_node = QgsLayerTreeGroup(compare_group_name)
+    layer_group_node.setGroupLayer(group_layer)
 
+    layer_group_node.addLayer(mask_layer)
+    root.insertChildNode(0, layer_group_node)
+        
     return layer_group_node, mask_layer
 
 
-def _is_in_group(layer, layer_group):
+def _is_in_group(layer:QgsMapLayer, layer_group:QgsMapLayer) -> bool:
     """Return True if a target layer is in a target layer groyp"""
     for child in layer_group.children():
         if child.layerId() == layer.id():  # 0 = Layer node
