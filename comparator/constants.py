4486ee9af1c2a7eba443b645f913811c3115490a
bordoray <bordoray@gmail.com>
2025-01-21 08:51:42 +0900

    Horizontal split geometry formula

--- comparator/constants.py
+++ comparator/constants.py
@@ -1,14 +1,14 @@
 compare_group_name = "QMapCompare_Group"
 compare_mask_layer_name = "QMapCompareMask"
 
-horizontal_split_geometry = """make_rectangle_3points(
+vertical_split_geometry = """make_rectangle_3points(
         make_point(x(@map_extent_center), y(@map_extent_center) - (@map_extent_height / 2)),
         make_point(x(@map_extent_center), y(@map_extent_center) + (@map_extent_height / 2)),
         make_point(x(@map_extent_center) - (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
         0)"""
 
-vertical_split_geometry = """make_rectangle_3points(
-        make_point(x(@map_extent_center), y(@map_extent_center) - (@map_extent_width / 2)),
-        make_point(x(@map_extent_center), y(@map_extent_center) + (@map_extent_width / 2)),
-        make_point(x(@map_extent_center) - (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
+horizontal_split_geometry = """make_rectangle_3points(
+        make_point(x(@map_extent_center) - (@map_extent_width / 2), y(@map_extent_center)),
+        make_point(x(@map_extent_center) + (@map_extent_width / 2), y(@map_extent_center) ),
+        make_point(x(@map_extent_center) + (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
         0)"""
