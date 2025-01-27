e9e18757eef60813733b64e6a99af804f8a1b819
bordoray <bordoray@gmail.com>
2025-01-27 12:28:19 +0900

    Lens geometry formula

--- comparator/constants.py
+++ comparator/constants.py
@@ -12,3 +12,6 @@ horizontal_split_geometry = """make_rectangle_3points(
         make_point(x(@map_extent_center) + (@map_extent_width / 2), y(@map_extent_center) ),
         make_point(x(@map_extent_center) + (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
         0)"""
+
+# lens radius is 15% of the map extent width
+lens_geometry = "buffer(@canvas_cursor_point, @map_extent_width * 0.15)"
