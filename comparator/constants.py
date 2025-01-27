db4b49b4def85a3e8078e31149241735bdcefd53
bordoray <bordoray@gmail.com>
2025-01-27 13:22:47 +0900

    Fix/ adapt splits geometry to Non-inverted mask

--- comparator/constants.py
+++ comparator/constants.py
@@ -4,13 +4,13 @@ compare_mask_layer_name = "QMapCompareMask"
 vertical_split_geometry = """make_rectangle_3points(
         make_point(x(@map_extent_center), y(@map_extent_center) - (@map_extent_height / 2)),
         make_point(x(@map_extent_center), y(@map_extent_center) + (@map_extent_height / 2)),
-        make_point(x(@map_extent_center) - (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
+        make_point(x(@map_extent_center) + (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
         0)"""
 
 horizontal_split_geometry = """make_rectangle_3points(
         make_point(x(@map_extent_center) - (@map_extent_width / 2), y(@map_extent_center)),
         make_point(x(@map_extent_center) + (@map_extent_width / 2), y(@map_extent_center) ),
-        make_point(x(@map_extent_center) + (@map_extent_width / 2), y(@map_extent_center) + (@map_extent_height / 2)),
+        make_point(x(@map_extent_center) - (@map_extent_width / 2), y(@map_extent_center) - (@map_extent_height / 2)),
         0)"""
 
 # lens radius is 15% of the map extent width
