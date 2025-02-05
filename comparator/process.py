a926e431577fbc59416e7047b06c1d71388491ff
bordoray <bordoray@gmail.com>
2025-02-05 15:57:55 +0900

    Implement background draft

--- comparator/process.py
+++ comparator/process.py
@@ -15,9 +15,11 @@ from qgis.PyQt.QtGui import QPainter
 from .constants import (
     compare_mask_layer_name,
     compare_group_name,
+    compare_background_layer_name,
     horizontal_split_geometry,
     vertical_split_geometry,
     lens_geometry,
+    compare_background_geometry,
 )
 from .utils import is_in_group, make_dynamic
 
@@ -30,6 +32,7 @@ def process_compare(compare_layers: list, compare_method: str) -> None:
     - compare layers (a list of QgsMapLayer)
     - compare_method: 'vertical' 'horizontal' or 'lens'
     """
+    project = QgsProject.instance()
 
     compare_layer_group, compare_mask_layer = _create_compare_layer_group_and_mask(
         compare_method
@@ -48,7 +51,28 @@ def process_compare(compare_layers: list, compare_method: str) -> None:
         if not is_in_group(layer, compare_layer_group):
             compare_layer_group.addLayer(layer)
 
-    # Symbolize mask with geometry generator
+    # Add white polygon layer for background
+    background_layer = QgsVectorLayer(
+        "Polygon?crs=EPSG:3857", compare_background_layer_name, "memory"
+    )
+    project.addMapLayer(background_layer, False)
+    compare_layer_group.addLayer(background_layer)
+    # symbolize with rectangle of map extent (geometry generator)
+    background_geometry_generator = QgsGeometryGeneratorSymbolLayer.create(
+        {
+            "geometryModifier": compare_background_geometry,
+            "geometry_type": 2,  # Polygon
+            "extent": "",
+            "color": "white",
+        }
+    )
+    background_layer_symbol = QgsFillSymbol.createSimple({})
+    background_layer_symbol.changeSymbolLayer(0, background_geometry_generator)
+    background_renderer = QgsSingleSymbolRenderer(background_layer_symbol)
+    background_layer.setRenderer(background_renderer)
+    background_layer.triggerRepaint()
+
+    # Symbolize mask layer with geometry generator
     # Orientation Fallback is vertical
     geometry_formula = vertical_split_geometry
     if compare_method == "horizontal":
@@ -61,10 +85,11 @@ def process_compare(compare_layers: list, compare_method: str) -> None:
             "geometryModifier": geometry_formula,
             "geometry_type": 2,  # Polygon
             "extent": "",
+            "color": "white",
         }
     )
 
-    symbol = QgsFillSymbol.createSimple({"color": "0,0,0,0"})
+    symbol = QgsFillSymbol.createSimple({})
     symbol.changeSymbolLayer(0, geometry_generator)
 
     #  Create the inverted polygon renderer
