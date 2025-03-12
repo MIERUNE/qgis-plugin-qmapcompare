d207003232728cf384836723460e12dac6139859
bordoray <bordoray@gmail.com>
2025-03-12 07:08:06 +0100

    Redo fix for test

--- comparator/process.py
+++ comparator/process.py
@@ -214,10 +214,10 @@ def compare_with_mapview(compare_layers: list) -> None:
     mirror_dock_widget = mirror_widget.parent().parent().parent()
     mirror_dock_widget.setWindowTitle(mirror_widget_name)
 
-    # # Hide close button to avoid bug when closing mirror window
-    # features = mirror_dock_widget.features()
-    # features = features & ~QDockWidget.DockWidgetClosable
-    # mirror_dock_widget.setFeatures(features)
+    # Hide close button to avoid bug when closing mirror window
+    features = mirror_dock_widget.features()
+    features = features & ~QDockWidget.DockWidgetClosable
+    mirror_dock_widget.setFeatures(features)
 
     # Add Map themes
     mapThemesCollection = QgsProject.instance().mapThemeCollection()
